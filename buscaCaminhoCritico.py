cf8da98188ffcfe5417dcfcc05153fad042af2a6
pedroamaral01 <pedro.estevao@aluno.ufop.edu.br>
2024-09-17 16:26:56 -0300

    update: criaGrafoDeDisciplinas

--- buscaCaminhoCritico.py
+++ buscaCaminhoCritico.py
@@ -12,7 +12,13 @@ def leArquivo(file):
     return linhas  # Retorna a lista de linhas após ler todas elas
     
 def criaGrafoDeDisciplinas(dados):
-    G = nx.Graph() # Usando um grafo direcionado para representar as dependências
+    G = nx.DiGraph() # Grafo direcionado
+    
+    # Adiciona os nós de s e t
+    G.add_node("s")
+    G.add_node("t")
+    
+    duracoes = {}
        
     for row in dados:
         codigo = row['Código']
@@ -22,14 +28,25 @@ def criaGrafoDeDisciplinas(dados):
         dependencias = row['Dependências'].split(',') if row['Dependências'] else []
         
         
-        G.add_node(codigo, name=nome, periodo = periodo, duration=duracao)
+        G.add_node(codigo, name=nome, periodo = periodo)
+        duracoes[codigo] = duracao # armazena a duracao de cada curso
         
+        has_dependencies = False
+        
+         # Adiciona arestas com base nas dependências
         for dep in dependencias:
             if dep.strip():  # Adiciona uma aresta apenas se a dependência não estiver vazia
-                G.add_edge(dep.strip(), codigo)
+                G.add_edge(dep, codigo, peso=duracoes[dep])
+                has_dependencies = True
+        
+        # Se a tarefa não tiver dependências, conecta ao nó de s
+        if not has_dependencies:
+            G.add_edge("s", codigo, peso = 0)  
+            
+        G.add_edge(codigo,"t", peso = duracao) # conecta todos os nós a t   
                 
-    return G                
-
+    return G
+            
 def main():
     while True:
         file = input("Informe o caminho para o arquivo (0 para sair): ")
@@ -45,9 +62,11 @@ def main():
             for node in grafo.nodes(data=True):
                 print(node)
             
-            print("\nArestas do grafo:")
-            for edge in grafo.edges():
-                print(edge)
+            print("\nArestas do grafo com peso (duração):")
+            for u, v, data in grafo.edges(data=True):  # Acessa o dicionário 'data' das arestas
+                print(f"Aresta de {u} para {v}, peso: {data['peso']}")   
+                
+            print("Nós no grafo:", grafo.nodes())          
 
         except FileNotFoundError:
             print("Arquivo não encontrado. Tente novamente.")
